a1f8a2d082889ea1a892a1098df546b5ef63c1a9
Laughingdyc <duyuchen6324333@qq.com>
2023-07-21 10:34:11 +0800

    feat: button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,92 @@
 <script setup lang="ts">
     import { ref } from 'vue';
-    const hello = ref<string>('hello World!')
+    const hello = ref<string>('button')
 </script>
 
 <template>
-    <div>{{ hello }}</div>
+    <div class="helloBtn">{{ hello }}</div>
+    <div class="helloAnimationBtn">{{ hello }}</div>
 </template>
 
 <style scoped lang="scss">
+    $btn-width: 150px;
+    $btn-height: 50px;
+    $clip-length: 30px;
+    $duration: 3s;
+    .helloBtn{
+        height: $btn-height;
+        width: $btn-width;
+        text-align: center;
+        line-height: $btn-height;
+        position: relative;
 
+        &::before,
+        &::after{
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            border: 1px #000 solid;
+            transition: all ease 0.3s;
+        }
+
+        &::before{
+            clip-path: inset( 0 ( $btn-width - $clip-length ) ( $btn-height - $clip-length ) 0 );
+        }
+
+        &::after{
+            clip-path: inset( ( $btn-height - $clip-length ) 0 0 ( $btn-width - $clip-length ) );
+        }
+
+        &:hover{
+            &::before,
+            &::after{
+                clip-path: inset( 0 0 0 0 );
+            }
+        }
+    }
+
+    @keyframes border-animation {
+        0%, 100%{
+            clip-path: inset( 0 $btn-width 0 0 );
+        }
+        25%{
+            clip-path: inset( 0 0 $btn-height 0 );
+        }
+        50%{
+            clip-path: inset( 0 0 0 $btn-width );
+        }
+        75%{
+            clip-path: inset( $btn-height 0 0 0 );
+        }
+    }
+
+    .helloAnimationBtn{
+        height: $btn-height;
+        width: $btn-width;
+        text-align: center;
+        line-height: $btn-height;
+        position: relative;
+        margin-top: 30px;
+
+        &::before,
+        &::after{
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            border: 1px #000 solid;
+            animation: border-animation $duration linear infinite;
+        }
+
+        &::after{
+            animation-delay: $duration * -0.5;
+        }
+    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import '@/App.scss'
 import routes from './routes/router'
 
 const router = createRouter({
-    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    // 使用的 hash 模式
     history: createWebHashHistory(),
-    routes, // `routes: routes` 的缩写
+    routes,
 })
 createApp(App)
     .use(router)
